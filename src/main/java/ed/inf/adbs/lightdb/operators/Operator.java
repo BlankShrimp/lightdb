983cfddec97cb286c4954ec9f11c55393ffba852
BlankShrimp <vanishing.19@gmail.com>
2021-03-07 22:57:26 +0000

    Implement Scan Operator.

--- src/main/java/ed/inf/adbs/lightdb/operators/Operator.java
+++ src/main/java/ed/inf/adbs/lightdb/operators/Operator.java
@@ -2,13 +2,11 @@ package ed.inf.adbs.lightdb.operators;
 
 public abstract class Operator {
 
-    private int index = 0;
+    protected int index = 0;
 
     public Operator() {}
 
-    public abstract void getNextTuple();
+    public abstract String[] getNextTuple();
 
-    public void reset(){
-        index = 0;
-    };
+    public void reset(){ index = 0; }
 }
--- src/main/java/ed/inf/adbs/lightdb/operators/ScanOperator.java
+++ src/main/java/ed/inf/adbs/lightdb/operators/ScanOperator.java
@@ -1,8 +1,70 @@
 package ed.inf.adbs.lightdb.operators;
 
-public class ScanOperator extends Operator{
-    @Override
-    public void getNextTuple() {
+import java.io.*;
+import java.util.logging.Logger;
 
+public class ScanOperator extends Operator{
+
+    private FileInputStream fis;
+    private BufferedReader bufferedReader;
+    private String filename;
+
+    public ScanOperator(String filename) {
+        this.filename = filename;
+        try {
+            fis = new FileInputStream(filename);
+            bufferedReader = new BufferedReader(new InputStreamReader(fis));
+        } catch (FileNotFoundException e) {
+            Logger logger = Logger.getGlobal();
+            logger.severe(e.toString());
+        }
+    }
+
+    /**
+     * Emit next line of a given file. Calling this function will pull the
+     * next line to the caller.
+     * @return An array of String referring to one tuple.
+     */
+    @Override
+    public String[] getNextTuple() {
+        try {
+            String str;
+            if ((str = bufferedReader.readLine()) != null)
+                return str.split(",");
+        } catch (IOException e) {
+            Logger logger = Logger.getGlobal();
+            logger.severe(e.toString());
+        }
+        return null;
+    }
+
+    /**
+     * Reset the pointer to the beginning of the file.
+     */
+    @Override
+    public void reset() {
+        closeFile();
+        try {
+            fis = new FileInputStream(filename);
+            bufferedReader = new BufferedReader(new InputStreamReader(fis));
+        } catch (FileNotFoundException e) {
+            Logger logger = Logger.getGlobal();
+            logger.severe(e.toString());
+        }
+    }
+
+    /**
+     * This function should be called before exiting program.
+     */
+    public void closeFile() {
+        try {
+            if (fis != null) {
+                fis.close();
+                bufferedReader.close();
+            }
+        } catch (IOException e) {
+            Logger logger = Logger.getGlobal();
+            logger.severe(e.toString());
+        }
     }
 }
